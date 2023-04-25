edb589a10a7615df5ac2f78e8b0f2364396a3630
JamieSlinger <118739565+JamieSlinger@users.noreply.github.com>
2023-04-25 23:42:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ menu.onclick = () => {
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
+
+//animations
+
+const tl= gsap.timeline({defaults: {ease: "power1.out"}});
+
+
+tl.to('header',{x: "0%", duration:1});
+
+tl.to('.home-text',{x: "0%", duration:1},"-=1");
+
+//gsap.registerPlugin(ScrollTrigger);
+
+
+//tl.to('.services th,td',{x: "0%", duration:1, stagger:0.25, ScrollTrigger:{trigger: '#services', start:'top top'}});
+
